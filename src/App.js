c6f7c3582fb6304757e497c179012e6c595cc535
tangivan <38152920+tangivan@users.noreply.github.com>
2021-08-26 23:28:09 -0700

    added gif for demo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Redirect } from 'react-router-dom'
 import Analytics from './components/Analytics';
 import TimerList from './components/TimerList';
 import Navbar from './components/Navbar';
@@ -32,6 +32,7 @@ function App() {
             <Route path={`${process.env.PUBLIC_URL}/login`} component={Login} />
             <Route path={`${process.env.PUBLIC_URL}/forgot-password`} component={ForgotPassword} />
             <Route path={`${process.env.PUBLIC_URL}/link-account`} component={LinkAccount} />
+            <Redirect to='/' />
           </Switch>
         </AuthProvider>
       </Router>
