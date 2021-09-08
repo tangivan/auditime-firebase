c41834f1f7db1d68a429519c8a6cceab9f291e64
tangivan <38152920+tangivan@users.noreply.github.com>
2021-09-07 19:27:15 -0700

    added new utility helper function and updated TimerForm to use addNewTimer function.

--- src/utils/firebaseHelper.js
+++ src/utils/firebaseHelper.js
@@ -13,3 +13,20 @@ export const addDefaultTimer = (userID) => {
         }],
     })
 }
+
+export const addNewTimer = (name, userId) => {
+    return firebase.firestore().collection('users').doc(userId).collection('timers').add({
+        name,
+        timeShown: 0,
+        timeRunTotal: 0,
+        timerHistory: [{
+            events: 'created',
+            duration: 0,
+            timeStamp: Date.now()
+        }],
+    })
+}
+
+export const updateName = (name, userId, timerId) => {
+    return firebase.firestore().collection('users').doc(userId).collection('timers').doc(timerId).update({ name });
+}
